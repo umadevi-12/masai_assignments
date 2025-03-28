e37ebb696b5127ab4d07563982677b77f00453e3
umadevi-12 <bogathiu@gmail.com>
2025-03-28 23:38:09 +0530

    array manipulations done

---
+++ module1/day1/Array_Manipulation.js
@@ -0,0 +1,8 @@
+// Initialize an array with temperatures recorded over 7 days
+let temperatures = [30, 32, 31, 29, 33, 34, 35];
+
+// Access and update the temperature for the 4th day (index 3)
+temperatures[3] += 2;
+
+// Log the updated temperatures for all 7 days
+console.log("Updated Temperatures:", temperatures);
